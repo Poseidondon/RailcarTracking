656624eeb72694fa4db6b62178b7fc198072852d
Poseidondon <superzarub@rambler.ru>
2023-07-13 20:36:01 +0300

    Config file added + small refactoring

--- example.py
+++ example.py
@@ -1,22 +1,7 @@
 from railcar_tracker import count_objects
-from pprint import pprint
 
 model_path = 'models/yolov8n_v2.pt'
 video_path = 'video_examples/stat_4-2_720-30.mp4'
-kwargs = {
-    'conf': 0.6,
-    'iou': 0.4,
-    'tracker': 'customtrack_fast.yaml',
-    'show': True,
-    'line_width': 2,
-    'stream': True,
-    'debug': False,
-    'verbose': True
-}
-counter, railcars = count_objects(model_path, video_path, **kwargs)
-pprint(railcars)
+
+counter = count_objects(model_path, video_path)
 print(counter, '\nTotal:', sum(counter.values()))
-"""
-Counter({'hopper': 70, 'tank': 13, 'flatcar_bulkhead': 8, 'gondola': 4, 'container': 4, 'flatcar': 2}) 
-Total: 101
-"""
--- railcar_tracker/object_counter.py
+++ railcar_tracker/object_counter.py
@@ -1,15 +1,33 @@
+import yaml
+from yaml.loader import SafeLoader
 from ultralytics import YOLO
 from collections import Counter
 
 
-def count_objects(model_path, video_path, debug=False, **kwargs):
-    # Load a model
+def count_objects(model_path, video_path, config='tracker_cfg.yaml', **kwargs):
+    """
+    Count objects on video, using unique IDs
+
+    :param model_path: path to the model
+    :param video_path: path to the video
+    :param config: config name, inside of config/ folder
+    :param kwargs: overrides config parameters, see tracker_cfg.yaml
+    :return: Counter() object, containing counts for each class and total count
+    """
+
+    # load a model
     model = YOLO(model_path)
 
-    # Track with the model
+    # tracking
     railcars = {}
 
-    for result in model.track(video_path, **kwargs):
+    # load config
+    cfg = yaml.load(open(f'config/{config}'), Loader=SafeLoader)
+    for key in kwargs:
+        cfg[key] = kwargs[key]
+    debug = cfg.pop('debug')
+
+    for result in model.track(video_path, **cfg):
         if result.boxes.id is not None:
             ids, classes, confs = result.boxes.id.tolist(), result.boxes.cls.tolist(), result.boxes.conf.tolist()
             if debug:
@@ -23,4 +41,4 @@ def count_objects(model_path, video_path, debug=False, **kwargs):
 
     counter = Counter([model.names[i[0]] for i in railcars.values()])
 
-    return counter, railcars
+    return counter
